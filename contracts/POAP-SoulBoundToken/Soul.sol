a01c0b3d7d71834f5e0ac78057a3864f64850fd2
NeelClaudel <neel.claudel@gmail.com>
2023-02-06 16:59:41 +0100

    add: POAP soulbound

---
+++ contracts/POAP-SoulBoundToken/Soul.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract SOUL is ERC1155Supply, Ownable{
+    using Strings for uint256;
+
+    /* ============ Events ============ */
+    event EventMinterAdded(address indexed newMinter);
+    event EventMinterRemoved(address indexed oldMinter);
+
+    /* ============ Modifiers ============ */
+    /**
+     * Only minter.
+     */
+    modifier onlyMinter() {
+        require(_minters[_msgSender()], "must be minter");
+        _;
+    }
+
+    /* ============ State Variables ============ */
+    string private _name; // collection name
+    string private _symbol; // collection symbol
+    string private _baseURI; // collection base URI
+    mapping(address => bool) private _minters; // Minters
+
+    /* ============ Constructor ============ */
+    constructor(string memory name_, string memory symbol_) ERC1155(""){
+        _name = name_;
+        _symbol = symbol_;
+        _minters[msg.sender] = true;
+    }
+
+    /* ============ Public Functions ============ */
+    /**
+     * @dev Returns the token collection name.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the token collection symbol.
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the metadata uri for specified token id. The id need to exist.
+     */
+    function uri(uint256 eventId) public view virtual override returns (string memory) {
+        require(exists(eventId));
+        return bytes(_baseURI).length > 0 ? string(abi.encodePacked(_baseURI, eventId.toString())) : "";
+    }
+
+    /**
+     * @dev Returns whether an address has minter role.
+     */
+    function isMinter(address addr) public view returns(bool){
+        return _minters[addr];
+    }
+
+    /* ============ External Functions ============ */
+    /**
+     * @dev Mint SOUL. This function can only be called by minter.
+     */
+    function mint(address to, uint256 eventId)
+        external
+        onlyMinter
+    {
+        _mint(to, eventId, 1, "");
+    }
+
+    function setbaseURI(string memory baseURI_) external onlyOwner {
+        _baseURI = baseURI_;
+    }
+
+    /**
+     * @dev Add a new minter.
+     */
+    function addMinter(address minter) external onlyOwner {
+        require(minter != address(0), "Minter must not be 0 address");
+        require(!_minters[minter], "Minter already exist");
+        _minters[minter] = true;
+        emit EventMinterAdded(minter);
+    }
+
+    /**
+     * @dev Remove a existing minter.
+     */
+    function removeMinter(address minter) external onlyOwner {
+        require(_minters[minter], "Minter does not exist");
+        _minters[minter] = false;
+        emit EventMinterRemoved(minter);
+    }
+
+    /**
+     * @dev Soulbound version of {IERC1155-safeTransferFrom}. Tokens can only be transfered by contract owner under the approval of the token nowner.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 eventId,
+        uint256 amount,
+        bytes memory data
+    ) public override onlyOwner{
+        super.safeTransferFrom(from, to, eventId, amount, data);
+    }
+
+    /**
+     * @dev Soulbound version of {IERC1155-safeBatchTransferFrom}. Tokens can only be transfered by contract owner under the approval of the token nowner.
+     */
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory eventIds,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public override onlyOwner{
+        super.safeBatchTransferFrom(from, to, eventIds, amounts, data);
+    }
+}
---
+++ contracts/POAP-SoulBoundToken/SoulMinter.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./SOUL.sol";
+
+contract SOULMinter is Ownable{
+    /* ============ Events ============ */
+    event SOULMinted(address indexed account, uint256 indexed eventId);
+    event SignerChanged(address indexed oldSigner, address indexed newSigner);
+
+    /* ============ State Variables ============ */
+    address public signer;
+    SOUL public soul;
+    mapping(uint256 => mapping(address => bool)) public mintedAddress;   // 记录已经mint的地址
+
+    /* ============ Constructor ============ */
+    /*
+     * @dev initialize SOUL addresss and signer address
+     */
+    constructor(address soulAddr_, address signer_){
+        soul = SOUL(soulAddr_);
+        signer = signer_;
+    }
+
+    /* ============ Public Functions ============ */
+    /*
+     * @dev concatenate mint account and eventId to msgHash
+     * @param account: mint address
+     * @param eventId: ERC1155 token id
+     */
+    function getMessageHash(address account, uint256 eventId) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(account, eventId));
+    }
+
+    /**
+     * @dev Verify signature using ECDSA, return true if the signature is valid.
+     */
+    function verify(bytes32 ethSignedMessageHash, bytes memory signature) public view returns (bool) {
+        return ECDSA.recover(ethSignedMessageHash, signature) == signer;
+    }
+
+    /* ============ External Functions ============ */
+    
+    /**
+     * @dev mint token `eventId` to `account` if `signature` is valid. 
+     * `msgHash` is concatenated by `eventId` and `account`.
+     * @param account: mint account
+     * @param eventId: ERC1155 token id
+     */
+    function mint(address account, uint256 eventId, bytes memory signature)
+    external
+    {
+        bytes32 msgHash = getMessageHash(account, eventId); // 将account和eventId打包消息
+        bytes32 ethSignedMessageHash = ECDSA.toEthSignedMessageHash(msgHash); // 计算以太坊签名消息
+        require(verify(ethSignedMessageHash, signature), "Invalid signature"); // ECDSA检验通过
+        require(!mintedAddress[eventId][account], "Already minted!"); // 地址没有mint过
+        soul.mint(account, eventId); // mint
+        mintedAddress[eventId][account] = true; // 记录mint过的地址
+        emit SOULMinted(account, eventId);
+    }
+
+    /**
+     * @dev change signer address. Only owner can call.
+     * @param newSigner: address of new signer
+     */
+     function setSigner(address newSigner) external onlyOwner{
+         emit SignerChanged(signer, newSigner);
+         signer = newSigner;
+     }
+}
